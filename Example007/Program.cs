67ddc05ba0bd3a7e1897db3a028b53697b31d5b5
Ilya <maikvazowski@bk.ru>
2022-09-07 20:14:22 +0500

    Example008

--- Example007/Program.cs
+++ Example007/Program.cs
@@ -3,8 +3,8 @@
 //Console.WriteLine("+");
 
 int xa = 1, ya = 1, 
-    xb = 1, yb = 10,
-    xc = 80, yc = 10;
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
 
 Console.SetCursorPosition(xa, ya); 
 Console.WriteLine("+");   
